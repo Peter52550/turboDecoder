815d77cb555a4b8f5143893cfa43b9dc0efcb5e4
RT <yuantseng@gmail.com>
2021-05-06 22:39:23 +0800

    idk wtf i do

--- src/siso.v
+++ src/siso.v
@@ -5,29 +5,29 @@ module Siso(
              data_o
              );
 
+  /*=======================param declaration========================== */	
+  parameter data_size   = 10;
+  parameter neg_inf     = -512; // - 2^(data_size-1) 
+  parameter input_size  = 5;
+  parameter block_size  = 21;   // 3 * (input_size+2)
+
   /*========================IO declaration============================ */	  
   input                             clk_p_i;
   input                             reset_n_i;
   input   signed  [input_size-1:0]  enc_i;
   input   signed  [input_size-1:0]  sys_i;
   input   signed  [input_size-1:0]  ext_i;
-  output          [15:0]            data_o;
+  output  signed	[data_size-1:0]	  LLR           [0:block_size-1];
+  assign  data_o = LLR;
 
-
-  parameter data_size = 10;
-  parameter neg_inf   = -512;   // - 2^(data_size-1) 
-  parameter input_size = 5;
-  parameter block_size = 21;    // 3 * (input_size+2)
   /* =======================REG & wire================================ */
 	reg					[1:0]						state, state_nxt;
 	reg         								in, out;
-	reg signed	[data_size-1:0]	branch_metrics 			[0:block_size-1] [0:3] [0:3];
-	reg signed	[data_size-1:0]	forward_metrics 		[0:block_size] [0:3];
-	reg signed	[data_size-1:0]	backward_metrics 		[0:block_size] [0:3];
+	reg signed	[data_size-1:0]	branch_metrics 			[0:block_size-1]  [0:3] [0:3];
+	reg signed	[data_size-1:0]	forward_metrics 		[0:block_size]    [0:3];
+	reg signed	[data_size-1:0]	backward_metrics 		[0:block_size]    [0:3];
 	reg signed	[data_size-1:0]	LLR             		[0:block_size-1];
-	reg 				[1:0]           past1, past2
-	reg					[1:0]						next1, next2;
-	reg					[1:0]           current;
+  
   /* ====================Combinational Part================== */
   //next-state logic //todo
 	assign next1 = {0, current[1]};
