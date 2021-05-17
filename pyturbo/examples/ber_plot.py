92ee90fe8a1f86813340fbfc29fcadbb385c900e
RT <yuantseng@gmail.com>
2021-05-17 17:08:29 +0800

    add pattern generation py file & fix most of siso.v (overflow not yet fixed)

--- pyturbo/examples/ber_plot.py
+++ pyturbo/examples/ber_plot.py
@@ -8,32 +8,18 @@ import argparse
 import random
 import numpy as np
 import matplotlib.pyplot as plot
-import copy
+
 from turbo import TurboEncoder
 from turbo import AWGN
 from turbo import TurboDecoder
 from turbo import SISODecoder
 
-def convert(a, bit, bound):
-    # a is a np.array
-    # -----(-1)--|----|----|--(1)-----
-    segments = 2**bit
-    arr = copy.copy(a)
-    segment_len = (bound - (-bound)) / (segments-1)
-    arr[a > bound - segment_len * 0.5] = bound
-    arr[a < -bound + segment_len * 0.5] = -bound
-    for i in range(2, segments): # 2 to num of segments -1
-        arr[(a > -bound + segment_len * (i-2 + 1/2)) & (a < -bound + segment_len * (i-1 + 1/2))] = -bound + segment_len * (i-1)
-    return arr
 
 def create_ber_plot(plot_params):
     block_size = plot_params["block_size"]
-    #num_trials = plot_params["num_trials"]
-    #bits_num   = plot_params["bits_num"]
-    boundary   = plot_params["boundary"]
+    num_trials = plot_params["num_trials"]
 
     snr_range = np.linspace(*plot_params["snr"])
-    bit_range = np.linspace(*plot_params["bits_num"]).astype(np.int)
 
     interleaver = random.sample(range(0, block_size), block_size)
     encoder = TurboEncoder(interleaver)
@@ -42,13 +28,8 @@ def create_ber_plot(plot_params):
     coded_errors = np.zeros(len(snr_range))
     uncoded_errors = np.zeros(len(snr_range))
 
-    truncated_errors = []
-    for i in range(len(bit_range)):
-        truncated_errors.append(np.zeros(len(snr_range)))
-
     for n in range(len(snr_range)):
-        count = 0
-        while True:
+        for _ in range(num_trials):
             input_vector = np.random.randint(2, size=block_size)
             encoded_vector = encoder.execute(input_vector)
 
@@ -56,64 +37,31 @@ def create_ber_plot(plot_params):
 
             channel_vector = list(map(float, encoded_vector))
             channel_vector = channel.convert_to_symbols(channel_vector)
+
             uncoded_vector = list(map(float, input_vector))
             uncoded_vector = channel.convert_to_symbols(uncoded_vector)
+            uncoded_vector = channel.execute(uncoded_vector)
 
             channel_vector = channel.execute(channel_vector)
+
             decoded_vector = decoder.execute(channel_vector)
             decoded_vector = [int(b > 0.0) for b in decoded_vector]
-
-            for i in range(len(bit_range)):
-                if (total errors < 200):
-                    truncated_vector = convert(channel_vector, bit_range[i], boundary)
-                    truncated_vector = decoder.execute(truncated_vector)
-                    truncated_vector = [int(b > 0.0) for b in truncated_vector]
-
-            uncoded_vector = channel.execute(uncoded_vector)
             uncoded_vector = [int(b > 0.0) for b in uncoded_vector]
 
+            decoder.reset()
+
             coded_error_count = sum([x ^ y for x, y in zip(input_vector, decoded_vector)])
-            truncated_error_count_0 = sum([x ^ y for x, y in zip(input_vector, decoded_vector_0)])
-            truncated_error_count_1 = sum([x ^ y for x, y in zip(input_vector, decoded_vector_1)])
-            truncated_error_count_2 = sum([x ^ y for x, y in zip(input_vector, decoded_vector_2)])
-            truncated_error_count_3 = sum([x ^ y for x, y in zip(input_vector, decoded_vector_3)])
-            truncated_error_count_4 = sum([x ^ y for x, y in zip(input_vector, decoded_vector_4)])
-            truncated_error_count_5 = sum([x ^ y for x, y in zip(input_vector, decoded_vector_5)])
             uncoded_error_count = sum([x ^ y for x, y in zip(input_vector, uncoded_vector)])
 
             coded_errors[n] = coded_errors[n] + coded_error_count
-            truncated_errors_0[n] = truncated_errors_0[n] + truncated_error_count_0
-            truncated_errors_1[n] = truncated_errors_1[n] + truncated_error_count_1
-            truncated_errors_2[n] = truncated_errors_2[n] + truncated_error_count_2
-            truncated_errors_3[n] = truncated_errors_3[n] + truncated_error_count_3
-            truncated_errors_4[n] = truncated_errors_4[n] + truncated_error_count_4
-            truncated_errors_5[n] = truncated_errors_5[n] + truncated_error_count_5
             uncoded_errors[n] = uncoded_errors[n] + uncoded_error_count
 
         print("Finished {} trials for SNR = {:8.2f} dB ...".format(num_trials, snr_range[n]))
 
     coded_ber_values = coded_errors / (num_trials * block_size)
-    truncated_ber_values_0 = truncated_errors_0 / (num_trials * block_size)
-    truncated_ber_values_1 = truncated_errors_1 / (num_trials * block_size)
-    truncated_ber_values_2 = truncated_errors_2 / (num_trials * block_size)
-    truncated_ber_values_3 = truncated_errors_3 / (num_trials * block_size)
-    truncated_ber_values_4 = truncated_errors_4 / (num_trials * block_size)
-    truncated_ber_values_5 = truncated_errors_5 / (num_trials * block_size)
     uncoded_ber_values = uncoded_errors / (num_trials * block_size)
-    # print(truncated_ber_values_0)
-    # print(truncated_ber_values_1)
-    # print(truncated_ber_values_2)
-    # print(truncated_ber_values_3)
-    # print(uncoded_ber_values)
-    # print(coded_ber_values)
 
     plot.plot(snr_range, coded_ber_values, "r.-", label="Coded BPSK")
-    plot.plot(snr_range, truncated_ber_values_0, "g*-", label=f"Truncated w/ {bit_range[0]} bits and {boundary} bound BPSK")
-    plot.plot(snr_range, truncated_ber_values_1, "c*-", label=f"Truncated w/ {bit_range[1]} bits and {boundary} bound BPSK")
-    plot.plot(snr_range, truncated_ber_values_2, "m*-", label=f"Truncated w/ {bit_range[2]} bits and {boundary} bound BPSK")
-    plot.plot(snr_range, truncated_ber_values_3, "y*-", label=f"Truncated w/ {bit_range[3]} bits and {boundary} bound BPSK")
-    plot.plot(snr_range, truncated_ber_values_4, "k*-", label=f"Truncated w/ {bit_range[4]} bits and {boundary} bound BPSK")
-    plot.plot(snr_range, truncated_ber_values_5, "y^-", label=f"Truncated w/ {bit_range[5]} bits and {boundary} bound BPSK")
     plot.plot(snr_range, uncoded_ber_values, "bo-", label="Uncoded BPSK")
     plot.yscale("log")
     plot.title("Turbo Codes Performance for R=1/3, Block={}, Trials={}".format(block_size, num_trials))
@@ -134,7 +82,7 @@ def options():
     )
     parser.add_argument(
         "--block-size", type=int,
-        default=1000000,
+        default=100,
         help="Block size (size of interleaver)"
     )
     parser.add_argument(
@@ -150,8 +98,6 @@ if __name__ == "__main__":
     plot_params = {
         "snr": args.snr,
         "block_size": args.block_size,
-        "num_trials": args.num_trials,
-        "bits_num": [2,7,6],
-        "boundary": 8
+        "num_trials": args.num_trials
     }
     create_ber_plot(plot_params)
--- tb_verilog/tb_siso.v
+++ tb_verilog/tb_siso.v
@@ -1,149 +1,133 @@
 `timescale 1ns/10ps
-`define CYCLE    10
-`define INPUT       "../data/golden.dat"                         
-`define EXPECT      "../data/ans.dat"    
+`define CYCLE       10
+`define INPUT	    "../data/golden.dat"						 
+`define EXPECT	    "../data/ans.dat"	
 
 
 module siso_tb;
+	parameter INPUT_NUM = 10;
+	// parameter  [15:0] data_arr [0:4] = '{
+	//		 16'b1111_0010_1100_1111,
+	//		 16'b1111_0110_0100_1111,
+	//		 16'b1000_0011_1100_0001,
+	//		 16'b1001_1100_0101_1000,
+	//		 16'b0110_1010_0100_1100
+	//	 };
+	// parameter  [15:0] answer_arr [0:4] = '{
+	//		 16'b1111_0010_1100_1111,
+	//		 16'b1111_0110_0100_1111,
+	//		 16'b1000_0011_1100_0001,
+	//		 16'b1001_1100_0101_1000,
+	//		 16'b0110_1010_0100_1100
+	//	 };
 
-parameter ITERATE       = 10'd16;
-parameter INPUT_SIZE    = 4'd10;
+	reg				    clk, rst, over, stop;
+	reg   [83:0]		data;
+	reg   signed [28-1:0]		sys;
+	reg   signed [28-1:0]		enc;
+	reg   signed [84-1:0]		ext;
+	wire  [84-1:0]	    out;
+	reg				    en;
+	reg   [84-1:0]	    out_temp;	   
+	reg   [15:0]		err;
+	wire				done;
 
-// parameter  [15:0] data_arr [0:4] = '{
-//         16'b1111_0010_1100_1111,
-//         16'b1111_0110_0100_1111,
-//         16'b1000_0011_1100_0001,
-//         16'b1001_1100_0101_1000,
-//         16'b0110_1010_0100_1100
-//     };
-// parameter  [15:0] answer_arr [0:4] = '{
-//         16'b1111_0010_1100_1111,
-//         16'b1111_0110_0100_1111,
-//         16'b1000_0011_1100_0001,
-//         16'b1001_1100_0101_1000,
-//         16'b0110_1010_0100_1100
-//     };
+	reg   [83:0]	    input_mem	[0:INPUT_NUM-1];
+	reg   [83:0]		out_mem	    [0:INPUT_NUM-1];
 
-reg           clk, rst, over, stop;
-reg   [83:0]   data;
-reg   [6:0]   sys;
-reg   [6:0]   enc;
-reg   [9:0]   ext;
-wire  [9:0]   out;
-reg   [9:0]  out_temp;       
-reg   [15:0]  err;
-reg            done;
-
-reg   [83:0]   input_mem     ;
-reg   [9:0]    out_mem       [0:INPUT_SIZE-1];
-
-integer  iter, counter;
+	integer  iter, counter, pattern_num, out_f, out_error_f;
 
 
-Siso siso0(
-        .clk_i(clk),
-        .rst_n_i(rst), 
-        .read_en_i(en),
-        .sys_i(sys),
-        .enc_i(enc),
-        .ext_i(ext),
-        .data_o(out),
-        .done(done),
-    );       
-   
-initial	$readmemh (`INPUT,  input_mem);
-initial	$readmemh (`EXPECT, out_mem);
+	Siso siso0(
+		.clk_i(clk),
+		.reset_n_i(rst), 
+		.read_en_i(en),
+		.sys_i(sys),
+		.enc_i(enc),
+		.ext_i(ext),
+		.data_o(out),
+		.finish(done)
+	);	   
+	
+	initial	$readmemb (`INPUT,  input_mem);
+	initial	$readmemb (`EXPECT, out_mem);
 
-initial begin
-   clk         = 1'b1;
-   rst         = 1'b1;
-   stop        = 1'b0;
-   counter     = 0;
-   done        = 0;
-   iter        = 0;
-//    data        = data_arr[0];
-   pattern_num = 0; 
-    #2.5 reset=1'b0;                       
-    #2.5 reset=1'b1;
+	initial begin
+		clk		    = 1'b1;
+		rst		    = 1'b1;
+		stop        = 1'b0;
+		counter	    = 0;
+		iter		= 0;
+		ext		    = 0;
+		pattern_num = 0; 
+		//	data		= data_arr[0];
+		#2.5 rst=1'b0; 
+		#2.5 rst=1'b1;
+	end
 
-end
+	always begin #(`CYCLE/2) clk = ~clk; end
 
-
-always begin #(`CYCLE/2) clk = ~clk; end
-
-initial begin
-	$dumpfile("siso.fsdb");
-	$dumpvars;
-    out_f = $fopen("out.dat");
-    out_error_f = $fopen("out_error.dat");
-    if (out_f == 0) begin
-        $display("Output file open error !");
-        $finish;
-    end
-    else if (out_error_f == 0) begin
-        $display("Output error file open error !");
-        $finish;
-    end
-end
+	initial begin
+		$fsdbDumpfile("siso.fsdb");
+		$fsdbDumpvars("+mda");
+		out_f = $fopen("out.dat");
+		out_error_f = $fopen("out_error.dat");
+		if (out_f == 0) begin
+			$display("Output file open error !");
+			$finish;
+		end
+		else if (out_error_f == 0) begin
+			$display("Output error file open error !");
+			$finish;
+		end
+	end
 
 
-always @(negedge clk)begin
-    if(counter < INPUT_SIZE) begin
-        if(iter < 4) begin
-            // data        = input_mem[counter+1];
-            sys         = input_mem[counter][(83-iter*7):(77-iter*7)];
-            enc         = input_mem[counter][(55-iter*7):(49-iter*7)];
-            ext         = input_mem[counter][(27-iter*7):(21-iter*7)];
-            iter        = iter + 1;
-        end
-        else begin
-            counter     = counter+1;
-            iter        = 0;
-            
-        end
-        if(counter > 0)
-            out_temp    = out_mem[counter-1];         
-    end
-    else begin                                  
-       counter = 0;
-       stop = 1'd1;
-    end
-end
+	always @(negedge clk)begin
+		if(counter==0 || (counter < 5*INPUT_NUM && done)) begin
+			en = 1;
+			sys		= input_mem[counter][83:83-28+1];
+			//$display("%b", sys);
+			//$display("%b %b %b %b %b %b %b", $signed(sys[3:0]), $signed(sys[7:4]), $signed(sys[11:8]), $signed(sys[15:12]), $signed(sys[19:16]), $signed(sys[23:20]), $signed(sys[27:24]));
+			//$display("sys at time %d is %d %d %d %d %d %d %d", counter, $signed(sys[3:0]), $signed(sys[7:4]), $signed(sys[11:8]), $signed(sys[15:12]), $signed(sys[19:16]), $signed(sys[23:20]), $signed(sys[27:24]));
+			enc		= input_mem[counter][83-28:83-28*2+1];
+			//$display("enc at time %d is %d %d %d %d %d %d %d", counter, $signed(enc[3:0]), $signed(enc[7:4]), $signed(enc[11:8]), $signed(enc[15:12]), $signed(enc[19:16]), $signed(enc[23:20]), $signed(enc[27:24]));
+			counter = counter + 1;	
+			out_temp = out_mem[counter];	 
+		end
+		if(counter >= 5*INPUT_NUM)
+			stop = 1'd1;
+	end
 
-always @(posedge clk)begin
-    if(done) begin
-        if(out !== out_temp) begin
-            $display("ERROR at %d:output %d !=expect %d ", pattern_num, out, out_temp);
-            $fdisplay(out_error_f, "ERROR at %d:output %h !=expect %h ",pattern_num-2, out, out_temp);
-            err = err + 1;
-        end
-        else begin
-            $display("GREAT! You get %d and the output is %d", out, out_temp);
-        end
-        $fdisplay(out_f, "%d    output %h    expect %h ",pattern_num-2, out, out_temp);
-        pattern_num = pattern_num + 1; 
-        $display("---------------------------------------------\n");
-    end
-end
-initial begin
-      @(posedge stop)      
-      if(stop) begin
-        $display("---------------------------------------------\n");
-        $display("There are %d errors!\n", err);
-        $display("The total accuracy is %d/%d\n", err, INPUT_SIZE);
-        $display("---------------------------------------------\n");
-      end
-      else begin
-        $display("---------------------------------------------\n");
-        $display("-------------SIMULATION FUCK!!!-------------\n");
-        $display("-------------------FAIL-------------------\n");
-        $display("---------------------------------------------\n");
-    end
-      $finish;
-end
- 
-endmodule
- 
+	always @(posedge clk)begin
+		if(out !== out_temp) begin
+			//$display("ERROR at %d:output %d !=expect %d ", pattern_num, out, out_temp);
+			$fdisplay(out_error_f, "ERROR at %d:output %h !=expect %h ",pattern_num-2, out, out_temp);
+			err = err + 1;
+		end
+		else begin
+			$display("GREAT! You get %d and the output is %d", out, out_temp);
+		end
+		$fdisplay(out_f, "%d	output %h	expect %h ",pattern_num-2, out, out_temp);
+		pattern_num = pattern_num + 1; 
+		//$display("---------------------------------------------\n");
+	end
+	initial begin
+		@(posedge stop)	  
+		if(stop) begin
+			$display("---------------------------------------------\n");
+			$display("There are %d errors!\n", err);
+			$display("The total accuracy is %d/%d\n", err, INPUT_NUM);
+			$display("---------------------------------------------\n");
+		end
+		else begin
+			$display("---------------------------------------------\n");
+			$display("-------------SIMULATION FAIL!!!--------------\n");
+			$display("---------------------------------------------\n");
+		end
+		$finish;
+	end
+
 endmodule
 
 
