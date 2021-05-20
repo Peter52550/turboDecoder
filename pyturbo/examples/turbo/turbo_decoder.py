51e76cb052aff4c223cb2b8897bc9527775996ae
peter <b07901146@ntu.edu.tw>
2021-05-20 23:53:50 +0800

    add all

--- pyturbo/examples/turbo/turbo_decoder.py
+++ pyturbo/examples/turbo/turbo_decoder.py
@@ -52,26 +52,26 @@ class TurboDecoder:
 
     def iterate(self, vector, i):
         input_tuples = self.demultiplex(vector[::3], vector[1::3], self.LLR_ext)
-        # print("vector[::3]", vector[::3], [bins(int(i), 4) for i in vector[::3]])
-        # print("vector[1::3]", vector[1::3], [bins(int(i), 4) for i in vector[1::3]])
-        # print("self.LLR_ext", self.LLR_ext, [bins(int(i), 10) for i in self.LLR_ext])
-        # print("=====================================")
+        print("vector[::3]", vector[::3], [bins(int(i), 4) for i in vector[::3]])
+        print("vector[1::3]", vector[1::3], [bins(int(i), 4) for i in vector[1::3]])
+        print("self.LLR_ext", self.LLR_ext, [bins(int(i), 10) for i in self.LLR_ext])
+        print("=====================================")
         LLR_1 = self.decoders[0].execute(input_tuples, i)
 
-        # print("LLR_1 binary: ", LLR_1, [bins(int(i), 10) for i in LLR_1])
+        print("LLR_1 binary: ", LLR_1, [bins(int(i), 10) for i in LLR_1])
         LLR_1 = LLR_1 - self.LLR_ext - 2 * vector[::3]
         
         LLR_interleaved = self.interleave(LLR_1)
         input_interleaved = self.interleave(vector[::3])
         
-        # print("input_interleaved", input_interleaved, [bins(int(i), 4) for i in input_interleaved])
-        # print("vector[2::3]", vector[2::3], [bins(int(i), 4) for i in vector[2::3]])
-        # print("LLR_interleaved", LLR_interleaved, [bins(int(i), 10) for i in LLR_interleaved])
-        # print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("input_interleaved", input_interleaved, [bins(int(i), 4) for i in input_interleaved])
+        print("vector[2::3]", vector[2::3], [bins(int(i), 4) for i in vector[2::3]])
+        print("LLR_interleaved", LLR_interleaved, [bins(int(i), 10) for i in LLR_interleaved])
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         input_tuples = self.demultiplex(input_interleaved, vector[2::3], LLR_interleaved)
 
         LLR_2 = self.decoders[1].execute(input_tuples)
-        # print("LLR_2 binary ", LLR_2, [bins(int(i), 10) for i in LLR_2])
+        print("LLR_2 binary ", LLR_2, [bins(int(i), 10) for i in LLR_2])
         
         LLR_2 = LLR_2 - LLR_interleaved - 2 * input_interleaved
         self.LLR_ext = self.deinterleave(LLR_2)
